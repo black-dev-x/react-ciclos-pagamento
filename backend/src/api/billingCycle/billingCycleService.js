07946022b7029da572dfb8c558b9e38e39c61cf4
thiagodevx <thiago.dev.x@gmail.com>
2020-04-18 01:29:39 -0300

    criação do método de count

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -3,4 +3,13 @@ const BillingCycle = require('./billingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({ new: true, runValidators: true })
 
+BillingCycle.route('count', (request, response) => {
+  BillingCycle.count((error, value) => {
+    if (error) {
+      response.status(500).json({ errors: [error] })
+    } else {
+      response.json({ value })
+    }
+  })
+})
 module.exports = BillingCycle
