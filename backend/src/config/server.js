2f041583525a561220aa21ab831fba44738504fc
thiagodevx <thiago.dev.x@gmail.com>
2020-04-18 00:57:48 -0300

    utilizando mensagens de erro no Schema do mongoose

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -7,4 +7,5 @@ app.use(express.json())
 const router = express.Router()
 app.use('/api', router)
 billingCycleService.register(router, '/billingCycles')
+
 app.listen(3003, _ => console.log('Servidor esta rodando'))
