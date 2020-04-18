e03d2cd57b736d50bacccc6718134821670117b9
thiagodevx <thiago.dev.x@gmail.com>
2020-04-18 01:34:17 -0300

    adicionando a função summary

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -12,4 +12,33 @@ BillingCycle.route('count', (request, response) => {
     }
   })
 })
+BillingCycle.route('summary', (request, response) => {
+  BillingCycle.aggregate(
+    [
+      {
+        $project: {
+          credit: { $sum: '$credits.value' },
+          debt: { $sum: '$debts.value' }
+        }
+      },
+      {
+        $group: {
+          _id: null,
+          credit: { $sum: '$credit' },
+          debt: { $sum: '$debt' }
+        }
+      },
+      {
+        $project: { _id: 0, credit: 1, debt: 1 }
+      }
+    ],
+    (error, result) => {
+      if (error) {
+        response.status(500).json({ errors: [error] })
+      } else {
+        response.json(result[0] || { credit: 0, debt: 0 })
+      }
+    }
+  )
+})
 module.exports = BillingCycle
